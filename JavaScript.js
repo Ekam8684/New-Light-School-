8c0fbd62d49b8319a30b450584828e5e4c9a0d6e
ekam <ekamdeep8684@gmail.com>
2024-01-09 16:36:34 +0530

    gallery page done

--- JavaScript.js
+++ JavaScript.js
@@ -160,22 +160,5 @@ wrapperone.addEventListener("mouseover", () => clearInterval(intervalId));
 // Add mouseleave event listener to wrapper element to start auto sliding again
 wrapperone.addEventListener("mouseleave", autoSlide);
 
+// --------------------------------------------------------------------
 
-// -----------------------------------R-Form-----------------------------------------------------
-// const form = document.querySelector("form"),
-//     nextBtn = form.querySelector(".nextBtn"),
-//     backBtn = form.querySelector(".backBtn"),
-//     allInput = form.querySelectorAll(".first input");
-
-
-// nextBtn.addEventListener("click", () => {
-//     allInput.forEach(input => {
-//         if (input.value != "") {
-//             form.classList.add('secActive');
-//         } else {
-//             form.classList.remove('secActive');
-//         }
-//     })
-// })
-
-// backBtn.addEventListener("click", () => form.classList.remove('secActive'));
